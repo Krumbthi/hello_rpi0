e7d7e5928fad14d107d4f08016796c8a7d612852
Thilo <t.krumbholz@bechstein-digital.com>
2022-01-06 14:22:12 +0100

    added shared memory

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,31 @@ fn main() {
 
     let mut bme280 = BME280::new();
     bme280.init().unwrap();
-    
+    let shmem_flink = "environment.json";
+
+    let shmem = match ShmemConf::new().size(4096).flink(shmem_flink).create() {
+        Ok(m) => m,
+        Err(ShmemError::LinkExists) => ShmemConf::new().flink(shmem_flink).open()?,
+        Err(e) => {
+            eprintln!("Unable to create or open shmem flink {} : {}",shmem_flink, e);
+            return;
+        }
+    };
+
+    // Get pointer to the shared memory
+    let raw_ptr = shmem.as_ptr();
+
     loop {
         info!("----------------------------");
-        let measurements = bme280.measure().unwrap();
-        info!("Rel. humidity: {} %", measurements.humidity);
-        info!("Temperature:   {} C", measurements.temperature);
-        info!("Pressure:      {} Pa", measurements.pressure);
+        let meas = bme280.measure().unwrap();
+        info!("Rel. humidity: {} %", meas.humidity);
+        info!("Temperature:   {} C", meas.temperature);
+        info!("Pressure:      {} Pa", meas.pressure);
+        *raw_ptr = format!("{{\"Data\": {{\"Temperature\":{}, \"Humidity\":{}, \"Pressure\":{} }}}}", meas.temperature, meas.humidity, meas.pressure);
+
         thread::sleep(Duration::from_secs(120)); 
     }
+
+    std::process::exit(0);
 }
 
